808c608f1355b18448a2ce4740d2258460a16d36
Srivani Mula <145700153+Srivani-mula@users.noreply.github.com>
2026-01-10 23:42:08 +0530

    Update features.py

--- src/features.py
+++ src/features.py
@@ -7,15 +7,15 @@ def extract_logmel(audio, sr, n_mels=64, max_len=100):
         sr=sr,
         n_mels=n_mels,
         n_fft=1024,
-        hop_length=512
+        hop_length=160
     )
 
     logmel = librosa.power_to_db(mel)
 
-    # FIXED LENGTH (IMPORTANT)
+    # FIXED LENGTH (THIS IS CRITICAL)
     if logmel.shape[1] < max_len:
         pad_width = max_len - logmel.shape[1]
-        logmel = np.pad(logmel, ((0,0),(0,pad_width)), mode='constant')
+        logmel = np.pad(logmel, ((0, 0), (0, pad_width)), mode="constant")
     else:
         logmel = logmel[:, :max_len]
 
